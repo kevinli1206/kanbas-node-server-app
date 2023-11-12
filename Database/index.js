f12bfe4ff2fc6613bf322990c8c9fb2282b1caee
kevinli1206 <li.kevin1@northeastern.edu>
2023-11-11 19:46:46 -0500

    one more

--- Database/index.js
+++ Database/index.js
@@ -4,12 +4,6 @@ import assignments from "./assignments.json" assert { type: "json" };
 import users from "./users.json" assert { type: "json" };
 import grades from "./grades.json" assert { type: "json" };
 import enrollments from "./enrollments.json" assert { type: "json" };
-// import courses from "./courses.json"; 
-// import modules from "./modules.json";
-// import assignments from "./assignments.json";
-// import users from "./users.json";
-// import grades from "./grades.json";
-// import enrollments from "./enrollments.json";
 export default {
   courses,
   modules,
